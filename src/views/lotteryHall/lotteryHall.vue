1e66bca18814579a62f8f009d88c471ca6340ebf
wangxh30 <wangxh30@lenovo.com>
2018-07-23 18:10:45 +0800

    框架搭建完成

--- src/views/lotteryHall/lotteryHall.vue
+++ src/views/lotteryHall/lotteryHall.vue
@@ -4,27 +4,18 @@
       <span class="head-title" slot="headtitle"><img src="./images/mobile_logo.png" alt=""></span>
     </Navbar>
     <div class="content">
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
-      <h1>123</h1>
+      <van-swipe :autoplay="3000" :show-indicators="false">
+        <van-swipe-item><img src="./images/banner1.png" alt=""></van-swipe-item>
+        <van-swipe-item><img src="./images/banner2.png" alt=""></van-swipe-item>
+        <van-swipe-item><img src="./images/banner3.png" alt=""></van-swipe-item>
+        <van-swipe-item><img src="./images/banner4.png" alt=""></van-swipe-item>
+      </van-swipe>
+      <div class="nocice">
+        <router-link to="">
+          <van-icon name="info-o" />
+          <span>银行入款最高送3000元彩金！笔笔送！</span>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -52,5 +43,19 @@
   .content {
     margin-top: px2rem(100px);
     margin-bottom: px2rem(100px);
+    .van-swipe {
+      height: px2rem(300px);
+      img {
+        height: px2rem(300px);
+        width: 100%;
+      }
+    }
+    .notice {
+      display: block;
+      height: px2rem(87px);
+      line-height: px2rem(87px);
+      overflow: hidden;
+      background-color: #fff;
+    }
   }
 </style>
