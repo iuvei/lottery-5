3e4e9c3de8ba33829b7edfe2cd52d9c5687999e4
wangxh30 <wangxh30@lenovo.com>
2018-08-09 10:51:51 +0800

    组ä»输入框数据处理

---
+++ src/views/lotteryHall/components/footerBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="footerBar">
+		<div class="multipleCon"></div>
+		<div class="multipleConLine"></div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
