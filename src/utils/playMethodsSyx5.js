7d82172d166692e0f91a1ea4011503675d008c57
wangzq13 <wangzq13@lenovo.com>
2018-08-17 10:04:39 +0800

    '8.17'

--- src/utils/playMethodsSyx5.js
+++ src/utils/playMethodsSyx5.js
@@ -37,6 +37,40 @@ export default function playMethodsSyx5 (type, detial, selectedData) {
     selectedNum = selectedData
   }
 
+  // 三码&前三直选单式
+  // if(type === '三码' && detial === '前三直选单式') {
+  //   let oneTwo = [] // 符合1，2级的
+  //   let Three = [] // 符合1，2，3最终的有效数据
+  //   let firstNumArr = selectedData[0].data.map(item => item.label)
+  //   let secondNumArr = selectedData[1].data.map(item => item.label)
+  //   let thirdNumArr = selectedData[2].data.map(item => item.label)
+  //   // 处理1，2级的
+  //   for (let first = 0; first < firstNumArr.length; first++) {
+  //     for (let second = 0; second < secondNumArr.length; second++) {
+  //       if (secondNumArr[second] !== firstNumArr[first]) {
+  //         let item = [firstNumArr[first], secondNumArr[second]]
+  //         oneTwo.push(item)
+  //       }
+  //     }
+  //   }
+  //   // 处理1&2级 和 3级的
+  //   // console.log('1&2的')
+  //   // console.log(oneTwo)
+  //   oneTwo.forEach(item => {
+  //     for (let third = 0; third < thirdNumArr.length; third++) {
+  //       if (item.indexOf(thirdNumArr[third]) === -1) {
+  //         let list = [item.concat(thirdNumArr[third])]
+  //         Three.push(list)
+  //       }
+  //     }
+  //   })
+  //   // console.log('符合条件的')
+  //   // console.log(Three)
+  //   bittingNumber = Three.length
+  //   price = 2 * bittingNumber
+  //   selectedNum = selectedData
+  // }
+
   // 三码&前三组选复式
   if(type === '三码' && detial === '前三组选复式') {
     selectedNum = selectedData[0].data.map(item => item.label)
