ad5cd1ed75d9345fa234a718c45d4d9c5954601e
sinshune <sinshune@163.com>
2018-08-23 19:02:20 +0800

    '完成三级页面'

---
+++ src/views/home/memberManage.vue
@@ -0,0 +1,116 @@
+<!--
+  name: memberManage
+  desc: 个人中心 -> 代理中心 -> 会员管理
+-->
+<template>
+  <div class="memberManage">
+    <Navbar>
+      <router-link slot="headleft" to="/agentCenter">
+        <van-icon name="arrow-left"/>
+      </router-link>
+      <span slot="headtitle" class="btn-group">会员管理</span>
+    </Navbar>
+
+    <div class="content">
+      <table>
+        <tr>
+          <th>帐号</th>
+          <th>类型</th>
+          <th>登录时间</th>
+          <th>下级人数</th>
+        </tr>
+        <tr v-for="(item, index) in member" :key="index">
+          <td>{{item.accountNum}}</td>
+          <td>{{item.style}}</td>
+          <td>{{item.loginTime}}</td>
+          <td>{{item.subNum}}</td>
+        </tr>
+        <tr v-show="member.length!=0">
+          <td colspan="4">已显示全部内容</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/Navbar.vue'
+
+  export default {
+    name: 'memberManage',
+    data () {
+      return {
+        member: [
+          {
+            accountNum: '34132424',
+            style: '代理',
+            loginTime: '2018-09-07 18:28:23',
+            subNum: 3
+          }
+        ]
+      }
+    },
+    components: {
+      Navbar
+    },
+    methods: {
+      toPage (src) {
+        this.$router.push(src);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "@/styles/index.scss";
+
+  ul, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .content {
+    margin-top: px2rem(100px);
+    margin-bottom: px2rem(100px);
+    overflow: hidden;
+    background: #efeef4;
+
+    table, tr, th, td {
+      border-top: 1px solid #cccccc;
+      border-collapse: collapse;
+    }
+    table {
+      border-top: none;
+      width: 100%;
+      background: #ffffff;
+      border-spacing: 0;
+      text-align: center;
+      color: #666666;
+      font-size: px2rem(34px);
+      tr {
+        height: px2rem(88px);
+        &:nth-child(1) {
+          border-top: none;
+          background: #f3f3f3;
+          th {
+            border-top: none;
+            padding: 0;
+            font-weight: 400;
+          }
+        }
+        td {
+          font-size: px2rem(28px);
+          color: #000;
+          &:nth-child(1) {
+            color: rgb(51, 136, 255);
+          }
+          &:last-child {
+            color: #000;
+          }
+        }
+      }
+    }
+  }
+
+</style>
---
+++ src/views/home/subReportForm.vue
@@ -0,0 +1,153 @@
+<!--
+  name: subReportForm
+  desc: 个人中心 -> 代理中心 -> 下级报表
+-->
+<template>
+  <div class="agentReportForm">
+    <Navbar>
+      <router-link slot="headleft" to="/agentCenter">
+        <van-icon name="arrow-left"/>
+      </router-link>
+      <span slot="headtitle" class="btn-group">下级报表</span>
+      <span slot="headright" @click="show=true">
+        <span class="whichDay">今天<van-icon name="success" /></span>
+      </span>
+    </Navbar>
+
+    <div class="content">
+      <table>
+        <tr>
+          <th>帐号</th>
+          <th>类型</th>
+          <th>报表人数</th>
+          <th>盈利</th>
+        </tr>
+        <tr v-for="(item, index) in formList" :key="index">
+          <td>{{item.accountNum}}</td>
+          <td>{{item.style}}</td>
+          <td>{{item.peopleNum}}</td>
+          <td>{{item.profit}}</td>
+        </tr>
+      </table>
+      <p class="tips" v-if="!formList.length">
+        <van-icon name="points"/>
+        <span>暂无记录</span>
+      </p>
+    </div>
+
+    <!-- 弹出层 -->
+    <div class="popup">
+      <van-popup v-model="show" position="bottom">
+        <li>今天</li>
+        <li>昨天</li>
+        <li>本月</li>
+        <li>上月</li>
+        <li @click="show=false">取消</li>
+      </van-popup>
+    </div>
+    <!-- 弹出层 end -->
+  </div>
+</template>
+
+<script>
+  import Navbar from '@/components/Navbar.vue'
+
+  export default {
+    name: 'subReportForm',
+    data () {
+      return {
+        show: false,
+        formList: [
+          {
+            accountNum: '帐号3242',
+            style: '类型1',
+            peopleNum: 10,
+            profit: 1314.00
+          }
+        ]
+      }
+    },
+    components: {
+      Navbar
+    },
+    methods: {
+      toPage (src) {
+        this.$router.push(src);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "@/styles/index.scss";
+
+  ul, li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  /* 哪天 */
+  .whichDay {
+    font-size: px2rem(30px);
+  }
+  /* 哪天 end */
+
+  // @include onetoppx('.agentHeader', 'top', 'before', #ffffff)
+  .content {
+    margin-top: px2rem(100px);
+    overflow: hidden;
+    background: #efeef4;
+
+    table {
+      width: 100%;
+      background: #f3f3f3;
+      font-size: px2rem(34px);
+      color: #666666;
+      tr {
+        text-align: center;
+        &:nth-child(1) {
+          th {
+            width: 25%;
+            height: px2rem(88px);
+            font-weight: 400;
+          }
+        }
+
+        td {
+          font-weight: 100;
+          color: #333333;
+          font-size: px2rem(28px);
+        }
+      }
+    }
+
+    p.tips {
+      margin-top: px2rem(200px);
+      text-align: center;
+      font-size: px2rem(150px);
+      color: #bbbbbb;
+      span{
+        display: block;
+        font-size: px2rem(28px);
+      }
+    }
+  }
+
+  // 弹出层
+  @include onebottompx('.popup li', 'bottom', 'after', #ededed);
+  .popup{
+    li {
+      margin: 0;
+      padding: px2rem(30px);
+      list-style: none;
+      font-size: px2rem(40px);
+      text-align: center;
+      &:last-child {
+        border-top: px2rem(30px) solid #efeef4;
+      }
+    }
+  }
+  // 弹出层
+
+</style>
