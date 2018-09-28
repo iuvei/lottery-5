bd04eea3b174abcd3d6ee37f9e194b887fad36c8
wangxh30 <wangxh30@lenovo.com>
2018-09-28 21:26:35 +0800

    修复sscbug

--- src/permission.js
+++ src/permission.js
@@ -2,7 +2,7 @@ import router from './router'
 import store from './store'
 import {setActivePage, getActivePage,getToken} from './utils/auth'
 
-let routerMap = {'lotteryHall': 0, 'activity': 1, 'find': 2,'home': 4}
+let routerMap = {'lotteryHall': 0, 'activity': 1, 'find': 2, 'home': 3}
 
 router.beforeEach((to, from, next) => {
   window.scrollTo(0,0);
